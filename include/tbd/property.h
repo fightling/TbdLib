73a4fdc5b2a20b0472f18e8a882b9b0aec92acf2
patgithub <p.hoffmann@berlin.de>
2012-06-19 15:54:29 +0200

    new managed properties

--- include/tbd/property.h
+++ include/tbd/property.h
@@ -1,31 +1,49 @@
-#ifndef DECLARE_PROPERTY_H_
-#define DECLARE_PROPERTY_H_
+#ifndef TBD_PROPERTY_H_
+#define TBD_PROPERTY_H_
 
-namespace tbd
-{
+namespace tbd {
+  inline void null_monitor() {}
+}
+/** @defgroup tomoPropertyManaged managed properties
+ * @brief variable declaration including getter/setter/monitor
+ * @{
+ */ 
+#define TBD_PROPERTY_MON(type,name,monitor) \
+  private: type name##_; \
+  public:  void (name)(type const & _##name) { name##_=_##name; monitor(); } \
+           type (name)() const { return name##_; } \
+  private:
+
+#define TBD_PROPERTY_REF_MON(type,name,monitor) \
+  private: type name##_; \
+  public:  void (name)(type const & _##name) { name##_=_##name; monitor(); } \
+           const type& (name)() const { return name##_; } \
+	private:
+
+/// @}
+
+/** @defgroup tomoPropertyBasic basic properties
+ * @brief variable declaration including getter/setter w/o monitoring
+ * @{
+ */ 
 
 #define TBD_PROPERTY_RO(type,name) \
-	private:  type                                            name##_; \
-	public:   type (name)() const                      { return name##_; } \
-	private:
+  private: type name##_; \
+  public:  type (name)() const { return name##_; } \
+  private:
 
 #define TBD_PROPERTY(type,name) \
-	private: 	type 						name##_; \
-	public:   	void  (name)(type const & _##name)  { name##_=_##name; } \
-				type (name)() const        { return name##_; } \
-	private:
+  TBD_PROPERTY_MON(type,name,tbd::null_monitor);
 
 #define TBD_PROPERTY_REF(type,name) \
-	private: 	type 						name##_; \
-	public:   	void  (name)(type const & _##name) { name##_=_##name; } \
-				const type& (name)()  const   { return name##_; } \
-	private:
+  TBD_PROPERTY_REF_MON(type,name,tbd::null_monitor);
 
 #define TBD_PROPERTY_PTR(type,name) \
-	private: 	type* 						name##_; \
-	public:   	void  (name)(type* _##name)  { name##_=_##name; } \
-				    type* (name)()  const   { return name##_; } \
-	private:
-}
+  private: type* name##_; \
+	public:  void (name)(type* _##name) { name##_=_##name; } \
+          type* (name)() const { return name##_; } \
+  private:
 
-#endif /* DECLARE_PROPERTY_H_ */
+/// @}
+
+#endif /* TBD_PROPERTY_H_ */
